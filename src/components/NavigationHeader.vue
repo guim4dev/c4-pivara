41d9b1a8bb181e78ccce3fb0eb9b84a114ff8828
felipe.verderamis <felipe.verderamis@prima.com.br>
2022-08-30 17:20:32 -0300

    Header Navigation started flow

---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+  <div class="header-tracking">
+    <div class="header-center">
+      <NavigationHeaderItem iconName="notifications" itemName="Avisos"></NavigationHeaderItem>
+      <NavigationHeaderItem iconName="school" itemName="Turmas"></NavigationHeaderItem>
+      <NavigationHeaderItem iconName="account_circle" itemName="Alunos"></NavigationHeaderItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavigationHeaderItem from '@/components/NavigationHeaderItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavigationHeaderItem
+  }
+}
+  
+</script>
+    
+<style scoped>
+  .header-tracking{
+    overflow: hidden;
+    background-color: #ffffff;
+    padding: 30px 0px 30px 0px;
+  }
+
+  .header-center {
+    float: center;
+    margin-top: auto;
+  }
+
+  @media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: center;
+  }
+  .header-center {
+    float: none;
+  }
+}
+  
+</style>
+  
---
+++ src/components/NavigationHeaderItem.vue
@@ -0,0 +1,54 @@
+<template>
+    <a href="http://localhost:8081/avisos">
+        <figure class="icon"><figcaption  id="mouse">
+            <span class="material-symbols-outlined">{{ iconName }}</span></figcaption>
+            <figcaption id="mouse">{{ itemName }}</figcaption>
+        </figure>
+    </a>
+</template>
+
+<script>
+export default{
+    name:'NavigationHeaderItem',
+    props: {
+        iconName: {
+            type: String
+        },
+        itemName: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+   
+   .material-symbols-outlined {
+     font-size: 30px;
+     font-variation-settings:
+     'FILL' 0,
+     'wght' 400,
+     'GRAD' 0,
+     'opsz' 49
+   }
+
+   .header-tracking a {
+    font-family: 'Nunito', sans-serif;
+    font-weight: bolder;
+    font-size: 18px;
+    color: black;
+    text-align: center;
+    padding: 10px 50px 44px 50px;
+    line-height: 18px;
+  }
+
+  .icon {
+    width: 60px;
+    display: inline-block;
+  }
+
+  #mouse {
+      
+  }
+</style>
