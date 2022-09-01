1125417399154ad810a350b5cede708ecd9bab50
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-08-31 21:54:39 -0300

    fix import chartkick

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import Chartkick from 'vue-chartkick'
-import Chart from 'chart.js'
+import 'chartkick/chart.js'
 
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.use(ElementPlus)
-app.use(Chartkick.use(Chart))
+app.use(Chartkick)
 app.mount('#app')
